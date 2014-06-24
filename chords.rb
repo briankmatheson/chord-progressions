c815da584c3158ab3cda2b9e8c60cb627910c182
Brian Matheson <brian.k.matheson@gmail.com>
2014-06-24 15:42:37 -0400

    fix repeating chord logic and add ability to specify starting chord

--- chords.rb
+++ chords.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
 
 
-def build_progression (number_of_bars)
+def build_progression (number_of_bars, starting_chord)
 	chords = [ "a", "b", "c", "d", "e", "f", "g"]
+	starting_chord_index = chords.find_index(starting_chord)
 	progression = []
 	chord_indexes_used = []
 	include_second_and_sixth_chords = false
 	number_of_bars = number_of_bars - 1
-	chord_indexes = build_chord_indexes	
+	chord_indexes = build_chord_indexes(starting_chord_index)
 
 	progression.push chords[chord_indexes[0]]
+	chord_indexes_used.push 0
 
 	number_of_bars.times do 
 
@@ -38,8 +40,16 @@ def build_progression (number_of_bars)
 end
 
 def chords_are_repeated_too_much(next_chord_index, last_chord_index, chord_indexes_used)
-	return next_chord_index == chord_indexes_used[last_chord_index] && 
-	next_chord_index == chord_indexes_used[last_chord_index - 1]
+	if next_chord_index == chord_indexes_used[last_chord_index]
+		if chord_indexes_used.size < 1
+			return false 
+		else
+			if next_chord_index == chord_indexes_used[last_chord_index - 1]
+				return true
+			end
+		end
+	end
+	return false
 end
 
 def get_next_chord_index(include_second_and_sixth_chords, chord_indexes)
@@ -61,8 +71,14 @@ def get_first_chord_index
 	return random_number.to_i
 end
 
-def build_chord_indexes
-	first_chord_index = get_first_chord_index
+def build_chord_indexes(starting_chord_index)
+
+	if starting_chord_index
+		first_chord_index = starting_chord_index
+	else
+		first_chord_index = get_first_chord_index
+	end
+
 	second_chord_index = (first_chord_index + 1) % 7
 	fourth_chord_index = (first_chord_index + 3) % 7
 	fifth_chord_index = (first_chord_index + 4) % 7
@@ -79,4 +95,4 @@ def build_chord_indexes
 end
 
 
-puts build_progression(ARGV[0].to_i)
+puts build_progression(ARGV[0].to_i, ARGV[1])
